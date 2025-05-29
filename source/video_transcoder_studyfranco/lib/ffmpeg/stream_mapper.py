6416cac9ebcb75224e7e72c7fa9df777a500747e
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-05-29 11:38:18 +0000

    Test to add some parameters

--- source/video_transcoder_studyfranco/lib/ffmpeg/stream_mapper.py
+++ source/video_transcoder_studyfranco/lib/ffmpeg/stream_mapper.py
@@ -82,7 +82,12 @@ class StreamMapper(object):
         ]
 
         # Set default Main options
-        self.main_options = []
+        self.main_options = ['-map', '0',
+                             '-movflags', 'use_metadata_tags',
+                             '-map_metadata', '0',
+                             '-copy_unknown',
+                             '-c', 'copy',
+                             ]
 
         # Set default Advanced options
         self.advanced_options = [
