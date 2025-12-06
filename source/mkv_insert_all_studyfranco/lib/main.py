96ae6f6fcdbafa9ad23e224c09e11a88eb728075
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-12-06 16:42:29 +0100

    test relative path read

--- source/mkv_insert_all_studyfranco/lib/main.py
+++ source/mkv_insert_all_studyfranco/lib/main.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
         video.ffmpeg_pool_audio_convert = Pool(processes=tools.core_to_use)
         video.ffmpeg_pool_big_job = Pool(processes=1)
         
-        with open("titles_subs_group.json") as titles_subs_group_file:
+        with open("lib/titles_subs_group.json") as titles_subs_group_file:
             tools.group_title_sub = json.load(titles_subs_group_file)
 
         mergeVideo.merge_videos(args.file, args.source, args.out)
