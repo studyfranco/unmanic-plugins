e433ca4f21ad3150f017cf09ecf53070b2dba481
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-12-09 21:00:53 +0100

    Increase timeout

--- source/mkv_insert_all_studyfranco/lib/video.py
+++ source/mkv_insert_all_studyfranco/lib/video.py
@@ -267,7 +267,7 @@ class video():
                         nameFilesExtractCut.append(nameOutFile)
                         cmd = baseCommand.copy()
                         cmd.extend(["-map", "0:"+str(audio['StreamOrder']), nameOutFile])
-                        self.ffmpeg_progress_audio.append(ffmpeg_pool_audio_convert.apply_async(tools.launch_cmdExt_with_timeout_reload, (cmd,3,max(600*5,2400))))
+                        self.ffmpeg_progress_audio.append(ffmpeg_pool_audio_convert.apply_async(tools.launch_cmdExt_with_timeout_reload, (cmd,3,max(1200*5,7200))))
             else:
                 for audio in self.audios[language]:
                     if audio["compatible"]:
@@ -281,7 +281,7 @@ class video():
                             nameFilesExtractCut.append(nameOutFile)
                             cmd = baseCommand.copy()
                             cmd.extend(["-map", "0:"+str(audio['StreamOrder']), "-ss", cut[0], "-t", cut[1] , nameOutFile])
-                            self.ffmpeg_progress_audio.append(ffmpeg_pool_audio_convert.apply_async(tools.launch_cmdExt_with_timeout_reload, (cmd,3,max(600*5,2400))))
+                            self.ffmpeg_progress_audio.append(ffmpeg_pool_audio_convert.apply_async(tools.launch_cmdExt_with_timeout_reload, (cmd,3,max(1200*5,7200))))
                             cutNumber += 1
             
     def remove_tmp_files(self,type_file=None):
